16fe78496ec8a752bfde7c6e663f93de0ff9dd51
Fabiano Apoliano <fabianoapoliano@gmail.com>
2024-03-13 21:23:44 -0300

    Update commit

--- JavaScript/aulasJavaScript.js
+++ JavaScript/aulasJavaScript.js
@@ -162,33 +162,4 @@ carro.buzina(); //retorna o método buzina;
 console.log(carro.informacaoCompleta()); //retorna o método informacaoCompleta;
 
 
-//Eventos: ações disparadas pela interação do usário na página. São os eventos que tornam a página interativa e dinâmica;
-/*
-Exemplos:
-    onclick -> disparada quando recebe um click;
-    ondblclick - > disparado quando clique duplo;
-    onmouseover -> disparado quando o mouse está sobre o elemento;
-    onmouseout -> disparado quando o mouse é movido para fora do elemento;
-    onmousedown -> disparado quando o clique do botão for pressionado;
-    onmouseup -> disparado quando o clique do botão é liberado;
-    onchange -> disparado quando o conteúdo é alterado;
-    onload -> disparado quando a página termina de ser carregada;
-    onresize -> disparado quando a janela é redimencionada;
-*/
-
-
-                            /*Anotações da aula 3 de JavaScript.*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//os estudos continuam no index.js (testes de Eventos)
---
+++ JavaScript/index.js
@@ -0,0 +1,27 @@
+                            /*Anotações da aula 3 de JavaScript.*/
+
+//Eventos: ações disparadas pela interação do usário na página. São os eventos que tornam a página interativa e dinâmica;
+/*
+Exemplos:
+    onclick -> disparada quando recebe um click;
+    ondblclick - > disparado quando clique duplo;
+    onmouseover -> disparado quando o mouse está sobre o elemento;
+    onmouseout -> disparado quando o mouse é movido para fora do elemento;
+    onmousedown -> disparado quando o clique do botão for pressionado;
+    onmouseup -> disparado quando o clique do botão é liberado;
+    onchange -> disparado quando o conteúdo é alterado;
+    onload -> disparado quando a página termina de ser carregada;
+    onresize -> disparado quando a janela é redimencionada;
+*/
+
+//aqui começam os estudos junto com o index.html
+
+alert('Hello Word');
+
+//caminho relativo e caminho absoluto (verificar como utilizar no HTML)****
+
+
+
+
+
+
